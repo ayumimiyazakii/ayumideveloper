2228cc49b36e0c4442ab434eda0475550e96dfed
ayumimiyazakii <117886400+ayumimiyazakii@users.noreply.github.com>
2022-12-15 04:31:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function controlarMenu(){
+
+    if (menu_navegacao.style.display == 'none'){
+        menu_navegacao.style.display = 'flex'
+        botao_menu.style.display = 'none'
+    }
+    else{
+        menu_navegacao.style.display = 'none'
+        botao_menu.style.display = 'block'
+    }
+    
+}
